93ba1e311925b1f8ee0169e326b861ff1c838624
IamFastre <mahmoudmode057@gmail.com>
2024-06-01 13:58:44 +0300

    a `number` value is redundant, let's just use `float` for that

--- src/Runtime/Values/NumberValues.cs
+++ src/Runtime/Values/NumberValues.cs
@@ -2,12 +2,9 @@ using Enuii.Symbols.Typing;
 
 namespace Enuii.Runtime.Evaluation;
 
-public abstract class NumberValue(double value)
+public abstract class NumberValue
     : RuntimeValue
 {
-    public override object     Value { get; } = value == double.NegativeZero ? 0d : value;
-    public override TypeSymbol Type  { get; } = TypeSymbol.Number;
-
     public override string ToString()
         => Value.ToString()!.Replace('E', 'e')
          + (this is FloatValue ? "f" : "");
@@ -17,11 +14,18 @@ public abstract class NumberValue(double value)
         var db = double.Parse(value.Replace("f", "").Replace("F", ""));
         return floor ? new IntValue(db) : new FloatValue(db);
     }
+
+    public static NumberValue Get(double value, TypeID id)
+        => id == TypeID.Integer
+         ? new IntValue(value)
+         : id == TypeID.Float
+         ? new FloatValue(value)
+         : throw new Exception($"Can only use int for float not: {id}");
 }
 
 
 public sealed class IntValue(double value)
-    : NumberValue(value)
+    : NumberValue
 {
     public override object     Value { get; } = Math.Floor(value == double.NegativeZero ? 0d : value);
     public override TypeSymbol Type  { get; } = TypeSymbol.Integer;
@@ -29,7 +33,7 @@ public sealed class IntValue(double value)
 
 
 public sealed class FloatValue(double value)
-    : NumberValue(value)
+    : NumberValue
 {
     public override object     Value { get; } = value == double.NegativeZero ? 0d : value;
     public override TypeSymbol Type  { get; } = TypeSymbol.Float;
