e78d227c69ca38ccf9bdefb7588c4df414f7d1dd
IamFastre <mahmoudmode057@gmail.com>
2024-06-04 01:53:19 +0300

    protection against overflow

--- src/Runtime/Values/CharValue.cs
+++ src/Runtime/Values/CharValue.cs
@@ -6,7 +6,7 @@ namespace Enuii.Runtime.Evaluation;
 public sealed class CharValue(char value)
     : RuntimeValue
 {
-    public override object     Value { get; } = value;
+    public override object     Value { get; } = (char)(value % (char.MaxValue + 1));
     public override TypeSymbol Type  { get; } = TypeSymbol.Char;
 
     public override string ToString()
